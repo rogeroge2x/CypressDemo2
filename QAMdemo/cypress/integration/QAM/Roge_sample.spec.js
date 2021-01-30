4d0db9358d7a91b5b2f43a7f801876e2958ce4fe
ricoy <rogelyn.icoy@smartstart.us>
2021-01-30 11:07:45 +0800

    added something roge part3

---
+++ QAMdemo/cypress/integration/QAM/Roge_sample.spec.js
@@ -0,0 +1,10 @@
+describe('Login',function(){
+    it('Sign in',function(){
+        cy.visit('https://react-redux.realworld.io/#/login')
+        cy.get('input[type="email"]').type('tyne2021git@gmail.com')
+        cy.get('input[type="password"]').type('tyne1232021')
+        cy.get('.btn').contains('Sign in').should('be.visible').click()
+    })
+
+    Hellllooooo
+})
