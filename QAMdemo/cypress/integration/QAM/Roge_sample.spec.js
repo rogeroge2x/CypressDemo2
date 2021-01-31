2830fcf686744e2b1f57b83f42e975384be322b6
ricoy <rogelyn.icoy@smartstart.us>
2021-01-31 11:03:46 +0800

    sample force push

--- QAMdemo/cypress/integration/QAM/Roge_sample.spec.js
+++ QAMdemo/cypress/integration/QAM/Roge_sample.spec.js
@@ -3,8 +3,5 @@ describe('Login',function(){
         cy.visit('https://react-redux.realworld.io/#/login')
         cy.get('input[type="email"]').type('tyne2021git@gmail.com')
         cy.get('input[type="password"]').type('tyne1232021')
-        cy.get('.btn').contains('Sign in').should('be.visible').click()
     })
-
-    Hellllooooo
 })
